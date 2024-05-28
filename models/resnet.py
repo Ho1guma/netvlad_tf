cf6f7727e71b41c283533c3dee7357bb123ac080
hhhyyeee <changeling626@gmail.com>
2024-05-28 03:19:05 +0000

    240528: Remove NetVLAD layer from ResNetTypeI

--- models/resnet.py
+++ models/resnet.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from config import NUM_CLASSES
 from models.residual_block import make_basic_block_layer, make_bottleneck_layer
 
+
 def netVLAD(inputs, num_clusters, assign_weight_initializer=None,
             cluster_initializer=None, skip_postnorm=False):
     ''' skip_postnorm: Only there for compatibility with mat files. '''
@@ -25,7 +26,6 @@ def netVLAD(inputs, num_clusters, assign_weight_initializer=None,
     v = v / tf.sqrt(tf.math.reduce_sum(v ** 2, axis=-1, keepdims=True)
                     + 1e-12)
 
-
     return v
 
 def matconvnetNormalize(inputs, epsilon):
@@ -33,7 +33,7 @@ def matconvnetNormalize(inputs, epsilon):
                             + epsilon)
 
 class ResNetTypeI(tf.keras.Model):
-    def __init__(self, layer_params):
+    def __init__(self, layer_params, num_clusters=32):
         super(ResNetTypeI, self).__init__()
 
         self.conv1 = tf.keras.layers.Conv2D(filters=64,
@@ -56,14 +56,17 @@ class ResNetTypeI(tf.keras.Model):
         self.layer4 = make_basic_block_layer(filter_num=512,
                                              blocks=layer_params[3],
                                              stride=2)
-        self.conv2 = tf.keras.layers.Conv2D(32, 1, use_bias=False)
-        self.softmax = tf.keras.layers.Softmax(axis=-1)
 
-        self.initializer = tf.initializers.he_normal()
-        self.C = tf.Variable(lambda: self.initializer(shape=(1, 1, 1, 512, 32)))
-        self.add = tf.keras.layers.Add()
-        self.mul = tf.keras.layers.Multiply()
-        self.flatten = tf.keras.layers.Flatten()
+        # # NetVLAD layer
+        # self.conv2 = tf.keras.layers.Conv2D(num_clusters, 1, use_bias=False)
+        # self.softmax = tf.keras.layers.Softmax(axis=-1)
+
+        # self.initializer = tf.initializers.he_normal()
+        # self.C = tf.Variable(lambda: self.initializer(shape=(1, 1, 1, 512, 32)))
+        # self.add = tf.keras.layers.Add()
+        # self.mul = tf.keras.layers.Multiply()
+        # self.flatten = tf.keras.layers.Flatten()
+
         #self.avgpool = tf.keras.layers.GlobalAveragePooling2D()
         #self.fc = tf.keras.layers.Dense(units=NUM_CLASSES, activation=tf.keras.activations.softmax)
 
@@ -77,17 +80,18 @@ class ResNetTypeI(tf.keras.Model):
         x = self.layer3(x, training=training)
         x = self.layer4(x, training=training) #(b,8,10,512)
         #tf.keras.backend.l2_normalize(x, axis=None)
-        s = self.conv2(x)
-        a = self.softmax(s)
-        a = tf.keras.backend.expand_dims(a, -2)  # (b,8,10,1,64)
-        x = tf.keras.backend.expand_dims(x, -1)
-        v = self.add([x,self.C])
-        v = self.mul([a,v])
-        v = tf.reduce_sum(v, axis=[1, 2])
-        #v = tf.transpose(v, perm=[0, 2, 1])
-        output = self.flatten(v)
 
-        return output
+        # s = self.conv2(x)
+        # a = self.softmax(s)
+        # a = tf.keras.backend.expand_dims(a, -2)  # (b,8,10,1,64)
+        # x = tf.keras.backend.expand_dims(x, -1)
+        # v = self.add([x,self.C])
+        # v = self.mul([a,v])
+        # v = tf.reduce_sum(v, axis=[1, 2])
+        # #v = tf.transpose(v, perm=[0, 2, 1])
+        # output = self.flatten(v)
+
+        return x
 
 
 class ResNetTypeII(tf.keras.Model):
